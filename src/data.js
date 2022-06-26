58d1ee26f4974036bb7892f28ec5c1a00ba96941
JoelHauser <joel.hauser@gmail.com>
2022-06-26 15:01:03 -0400

    update

--- src/data.js
+++ src/data.js
@@ -8,41 +8,41 @@ export const featuredPortfolio = [
   {
     id: 2,
     title: "Fork that Orc",
-    img: "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+    img: "https://raw.githubusercontent.com/JoelHauser/ReactPortfolio/develop/public/Assets/New%20Project.png",
     link: "https://www.google.com"
   },
   {
     id: 3,
     title: "Readme Gen",
-    img: "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
+    img: "https://raw.githubusercontent.com/JoelHauser/ReactPortfolio/develop/public/Assets/NicePng_pen-and-paper-png_767861.png",
     link: "https://www.google.com"
   },
-  {
-    id: 4,
-    title: "Coming Soon",
-    img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
-    link: "https://www.google.com"
-  }
+  // {
+  //   id: 4,
+  //   title: "Coming Soon",
+  //   img: "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
+  //   link: "https://www.google.com"
+  // }
 ];
 
 export const webPortfolio = [
   {
     id: 1,
     title: "Forecaster",
-    img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
-    link: "https://www.google.com"
+    img: "https://github.com/JoelHauser/Forecaster",
+    link: "https://github.com/JoelHauser/Forecaster"
   },
   {
     id: 2,
     title: "Tech Blog",
     img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
-    link: "https://www.google.com"
+    link: "https://besttechblog.herokuapp.com/"
   },
   {
     id: 3,
     title: "Password Gen",
     img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
-    link: "https://www.google.com"
+    link: "https://github.com/JoelHauser/the-best-password-gen"
   }
   // {
   //   id: 4,
